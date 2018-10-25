83f27a95b47f9ce01054e407220419df648f3eeb
itsaiub <itsaiub@gmail.com>
2018-10-26 02:28:36 +0600

    posting data to server

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,31 +1,47 @@
 import React, { Component } from 'react';
 
 import './NewPost.css';
+import Axios from 'axios';
 
+
+const POST_URL = "https://jsonplaceholder.typicode.com/posts";
 class NewPost extends Component {
-    state = {
-        title: '',
-        content: '',
-        author: 'Aiub'
-    }
+	state = {
+		title: '',
+		content: '',
+		author: 'Aiub'
+	}
 
-    render () {
-        return (
-            <div className="NewPost">
-                <h1>Add a Post</h1>
-                <label>Title</label>
-                <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
-                <label>Content</label>
-                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
-                <label>Author</label>
-                <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
-                    <option value="Max">Max</option>
-                    <option value="Manu">Manu</option>
-                </select>
-                <button>Add Post</button>
-            </div>
-        );
-    }
+	postDataHandler = () => {
+		const data = {
+			title: this.state.title,
+			body: this.state.content,
+			author: this.state.author
+		}
+		Axios.post(POST_URL, data)
+			.then(respond => {
+				console.log(respond);
+				
+			})
+	}
+
+	render () {
+		return (
+			<div className="NewPost">
+				<h1>Add a Post</h1>
+				<label>Title</label>
+				<input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
+				<label>Content</label>
+				<textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+				<label>Author</label>
+				<select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
+					<option value="Ayub">Ayub</option>
+					<option value="Khan">Khan</option>
+				</select>
+				<button onClick={this.postDataHandler} >Add Post</button>
+			</div>
+		);
+	}
 }
 
 export default NewPost;
