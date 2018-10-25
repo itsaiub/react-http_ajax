141436a3ee4287e69b7be03988677fcb17d7c2e6
itsaiub <itsaiub@gmail.com>
2018-10-26 01:30:57 +0600

    http request to get data and render to screen using axiom

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
+const POST_URL = "https://jsonplaceholder.typicode.com/posts";
 class Blog extends Component {
+
+  state = {
+    posts: []
+  }
+
+  componentDidMount() {
+    // fetch(POST_URL)
+    //   .then(response => response.json())
+    //   .then(data => console.log(data))
+
+
+  axios.get(POST_URL)
+   .then(response => {
+      this.setState({posts: response.data})
+      //console.log(response);
+   })
+
+
+}
+
     render () {
+      const posts = this.state.posts.map(post => {
+        return <Post key={post.id} title={post.title}/>
+      })
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
