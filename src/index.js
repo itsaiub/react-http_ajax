408e35e759ab627f984204682a79525745b5beca
itsaiub <itsaiub@gmail.com>
2018-10-26 02:48:24 +0600

    adding interceptors to execute code globally

--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Axios from 'axios';
+
+Axios.interceptors.request.use(request => {
+  console.log(request);
+  // Edit request config
+  return request;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+})
+
+Axios.interceptors.response.use(response => {
+  console.log(response);
+  // Edit request config
+  return response;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
