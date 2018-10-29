a795bf93d3ae5fd26de5778f0b435baa3df5b87f
itsaiub <itsaiub@gmail.com>
2018-10-29 15:57:40 +0600

    remove state

--- src/redux-counter/containers/Counter.js
+++ src/redux-counter/containers/Counter.js
@@ -4,37 +4,7 @@ import CounterControl from "../components/CounterControl/CounterControl";
 import CounterOutput from "../components/CounterOutput/CounterOutput";
 import * as actionTypes from "../../store/action";
 class Counter extends Component {
-  state = {
-    counter: 0
-  };
-
-  counterChangeHandler = (action, value) => {
-    switch (action) {
-      case "inc":
-        this.setState(prevState => {
-          return { counter: prevState.counter + 1 };
-        });
-        break;
-      case "dec":
-        this.setState(prevState => {
-          return { counter: prevState.counter - 1 };
-        });
-        break;
-      case "add":
-        this.setState(prevState => {
-          return { counter: prevState.counter + value };
-        });
-        break;
-      case "sub":
-        this.setState(prevState => {
-          return { counter: prevState.counter - value };
-        });
-        break;
-      default:
-        break;
-    }
-  };
-
+  
   render() {
     return (
       <div>
