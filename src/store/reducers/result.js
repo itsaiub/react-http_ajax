685aa6a313c8e064770fb4443b63974088bed66a
itsaiub <itsaiub@gmail.com>
2018-10-31 10:00:27 +0600

    bug fix

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,21 +1,28 @@
-import * as actionTypes from '../actions/actionTypes'
+import * as actionTypes from "../actions/actionTypes";
 import { updateObj } from "../utility";
-
-
 const initialState = {
   results: []
+};
+
+const deleteResult = (state, action) => {
+  const updatedArray = state.results.filter(
+    result => result.id !== action.resultElId
+  );
+  return updateObj(state, { results: updatedArray });
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-      return updateObj(state, { results: state.results.concat({ date: new Date().toDateString(), value: action.result})})
+      return updateObj(state, {
+        results: state.results.concat({
+          id: new Date(),
+          date: new Date().toDateString(),
+          value: action.result
+        })
+      });
     case actionTypes.DELETE_RESULT:
-      // const id = 2;
-      // const newArray = [...state.results]; // One way to remove element from array immutably
-      // newArray.splice(id, 1);
-      const updatedArray = state.results.filter(result => result.id !== action.resultElId)
-      return updateObj(state, { results: updatedArray})
+      return deleteResult(state, action);
     default:
       return state;
   }
